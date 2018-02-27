b5e0922df23536878051cf7a014b1d7e5dd3f913
The Danger <34897456+gigitygoog@users.noreply.github.com>
2018-02-27 13:28:11 -0700

    fixed

---
+++ constants.py
@@ -0,0 +1,16 @@
+reddit_user = ""
+reddit_pass = ""
+reddit_subs = ["AFBbot"]
+reddit_api = ""
+reddit_secret = ""
+
+debuglogin = False
+debugsearch = False
+
+
+createdb = False  # Creates tables for all bases.
+
+
+bot_signature = '''---\n\n
+^^I ^^am ^^a ^^bot. ^^To ^^rate ^^a ^^base ^^simply ^^include ^^the ^^word ^^"rate",
+^^the ^^base ^^name, ^^and ^^an ^^integer ^^between ^^1-10. ^^Ex: ^^I ^^rate ^^Langley ^^5.'''
