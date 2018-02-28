c6ee961025055ae24fc52796fd358ed489c1d74f
thedanger1 <34897456+thedanger1@users.noreply.github.com>
2018-02-27 21:50:40 -0700

    Added trigger and changed signature

--- constants.py
+++ constants.py
@@ -11,8 +11,8 @@ debugsearch = False
 createdb = False  # Creates tables for all bases.
 
 
-triggers = ['afbbot!', 'afbbot', '/u/afbbot']
+triggers = ['afbbot!', '!afbbot', 'afbbot', '/u/afbbot']
 bot_signature = '''---\n\n
 ^^I ^^am ^^a ^^bot. ^^To ^^rate ^^a ^^base ^^simply ^^include ^^the ^^word ^^"rate",
 ^^the ^^base ^^name, ^^and ^^an ^^integer ^^between ^^1-10. ^^Ex: ^^I ^^rate ^^Langley ^^5.\n\n
-^^To ^^call ^^me ^^without ^^rating, ^^say: ^^!AFBbot ^^/ ^^AFBbot ^^/ ^^/u/AFBbot ^^and ^^a ^^base ^^name.'''
+^^To ^^call ^^me ^^without ^^rating, ^^say: ^^AFBbot! ^^/ ^^AFBbot ^^/ ^^/u/AFBbot ^^and ^^a ^^base ^^name.'''
